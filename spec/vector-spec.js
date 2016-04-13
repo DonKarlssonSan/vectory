7593651c83e7b1a5bc8a6f534b21f4effc73fb41
Johan Karlsson <DonKarlssonSan@gmail.com>
2016-04-13 06:54:47 +0200

    Added tests, implemented more methods

--- spec/vector-spec.js
+++ spec/vector-spec.js
@@ -10,4 +10,62 @@ describe("Vector", function () {
     expect(sum.y).toBe(8);
   });
 
+  it("should add to", function () {
+    var v1 = new vector.Vector(2, 3);
+    var v2 = new vector.Vector(4, 5);
+
+    v1.addTo(v2);
+    expect(v1.x).toBe(6);
+    expect(v1.y).toBe(8);
+  });
+
+  it("should subtract", function () {
+    var v1 = new vector.Vector(2, 3);
+    var v2 = new vector.Vector(4, 5);
+
+    var diff = v1.sub(v2);
+    expect(diff.x).toBe(-2);
+    expect(diff.y).toBe(-2);
+  });
+
+  it("should subtract from", function () {
+    var v1 = new vector.Vector(2, 3);
+    var v2 = new vector.Vector(4, 5);
+
+    v1.subFrom(v2);
+    expect(v1.x).toBe(-2);
+    expect(v1.y).toBe(-2);
+  });
+
+  it("should set angle", function () {
+    var v1 = new vector.Vector(1, 0);
+    v1.setAngle(Math.PI / 2);
+
+    expect(v1.x).toBeCloseTo(0);
+    expect(v1.y).toBeCloseTo(1);
+  });
+
+  it("should set length", function () {
+    var v1 = new vector.Vector(1, 0);
+    v1.setLength(2);
+
+    expect(v1.x).toBe(2);
+    expect(v1.y).toBe(0);
+  });
+  
+  it("should multiply", function () {
+    var v1 = new vector.Vector(2, 3);
+    var result = v1.mult(3);
+
+    expect(result.x).toBe(6);
+    expect(result.y).toBe(9);    
+  });
+  
+  it("should divide", function () {
+    var v1 = new vector.Vector(6, 9);
+    var result = v1.div(3);
+
+    expect(result.x).toBe(2);
+    expect(result.y).toBe(3);    
+  });
 });
--- src/vector.js
+++ src/vector.js
@@ -11,6 +11,50 @@ class Vector {
       this.x + v.x,
       this.y + v.y);
   }
+
+  addTo(v) {
+    this.x += v.x;
+    this.y += v.y;
+  }
+
+  sub(v) {
+    return new Vector(
+      this.x - v.x,
+      this.y - v.y);
+  }
+  
+  subFrom(v) {
+    this.x -= v.x;
+    this.y -= v.y;
+  }
+  
+  mult(n) {
+    return new Vector(this.x * n, this.y * n);
+  }
+  
+  div(n) {
+    return new Vector(this.x / n, this.y / n);
+  }
+  
+  setAngle(angle) {
+    var length = this.getLength();
+    this.x = Math.cos(angle) * length;
+    this.y = Math.sin(angle) * length;
+  }
+  
+  setLength(length) {
+    var angle = this.getAngle();
+    this.x = Math.cos(angle) * length;
+    this.y = Math.sin(angle) * length;
+  }
+  
+  getAngle() {
+    return Math.atan2(this.y, this.x);
+  }
+  
+  getLength(){
+    return Math.sqrt(this.x * this.x + this.y * this.y);
+  }
 }
 
 exports.Vector = Vector;
